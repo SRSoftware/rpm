d3fb1b760e4be00a4e474209a1ea88ba4baed6a5
Stephan Richter <srichter@srsoftware.de>
2013-08-29 16:48:34 +0200

    altered css path

--- index.php
+++ index.php
@@ -27,7 +27,7 @@ function head(){
       <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
       <title>Rock Paper Mensa</title>
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-      <link href="bootstrap/css/bootstrap.min.css" rel="stylesheet" media="screen" />
+      <link href="bootstrap.min.css" rel="stylesheet" media="screen" />
       <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
       <!--[if lt IE 9]>
           <script src="bootstrap/js/html5shiv.js"></script>
